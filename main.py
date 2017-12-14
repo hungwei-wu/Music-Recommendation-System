423e7d1f44389f6ed5419138fe3b982d89c3d4de
kevche <kevche@umich.edu>
2017-12-14 17:33:20 -0500

    fill_matrix for spare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utilities import file_io
+from utilities import file_io, fill_matrix
 from sklearn.feature_extraction import FeatureHasher
 from sklearn.neighbors import KNeighborsClassifier
 import time
@@ -6,24 +6,23 @@ from preprocessing.preprocessor import Preprocessor
 from LyricsProcessor import LyricsProcessor
 
 if __name__ == "__main__":
-    #chunks = file_io.read_lastfm_user_art_file("data/userid-timestamp-artid-artname-traid-traname.tsv")
+    # chunks = file_io.read_lastfm_user_art_file("data/userid-timestamp-artid-artname-traid-traname.tsv")
     chunks = file_io.read_lastfm_user_art_file("data/test_shorter.tsv")
 
     # read songs
-    vectorizer = FeatureHasher()
+    vectorizer = FeatureHasher(n_features=1048576, non_negative=True)
     pre = Preprocessor(chunks, vectorizer)
     songs = pre.read_songs(20)
     print(songs)
 
     # reset file reader
-    #chunks = file_io.read_lastfm_user_art_file("data/tmp.tsv")
-    #pre.reset_file_reader(chunks)
+    # chunks = file_io.read_lastfm_user_art_file("data/tmp.tsv")
+    # pre.reset_file_reader(chunks)
 
     # read user song mapping
     pre.read_user_songs(1000)
     # convert to user-song matrix
     X = pre.get_user_song_matrix()
-    print (X.shape)
 
     start_time = time.time()
     clf = KNeighborsClassifier(n_neighbors=1)
@@ -31,23 +30,23 @@ if __name__ == "__main__":
     print(clf.predict(pre.user_song_dict["user_000001"]))
 
     print("training and predict using {0}".format(time.time() - start_time))
+    fill_matrix.fill_matrix(X)
 
-    
-    #song_content = [ (artist,song) for (artist,song) in zip(list(songs['artname']), list(songs['traname']))]
-    # temporary song_list
-    song_content = [('Underworld', 'Boy, Boy, Boy'),
-                    ('Underworld', 'Crocodile'),
-                    ('Led Zeppelin','Stairway to heaven'),
-                    ('Imagine Dragons','Thunder'),
-                    ('Sam Smith', 'Too Good At Goodbyes'),
-                    ('Ed Sheeran','Perfect'),
-                    ('Demi Lovato','Sorry Not Sorry'), 
-                    ('Pink','What About Us')
-                    ]
-    l_pre = LyricsProcessor(song_content)
-    l_pre.tfidf_transform()
-    l_pre.word2vec()
-    test1=l_pre.get_w2v_from_songname('Boy, Boy, Boy')
-    test2=l_pre.get_w2v_from_songname('Crocodile')
-    l_pre.write_song_word2vec()
-    
+    # # song_content = [ (artist,song) for (artist,song) in zip(list(songs['artname']), list(songs['traname']))]
+    # # temporary song_list
+    # song_content = [('Underworld', 'Boy, Boy, Boy'),
+    #                 ('Underworld', 'Crocodile'),
+    #                 ('Led Zeppelin', 'Stairway to heaven'),
+    #                 ('Imagine Dragons', 'Thunder'),
+    #                 ('Sam Smith', 'Too Good At Goodbyes'),
+    #                 ('Ed Sheeran', 'Perfect'),
+    #                 ('Demi Lovato', 'Sorry Not Sorry'),
+    #                 ('Pink', 'What About Us')
+    #                 ]
+    # l_pre = LyricsProcessor(song_content)
+    # l_pre.tfidf_transform()
+    # l_pre.word2vec()
+    # test1 = l_pre.get_w2v_from_songname('Boy, Boy, Boy')
+    # test2 = l_pre.get_w2v_from_songname('Crocodile')
+    # l_pre.write_song_word2vec()
+
