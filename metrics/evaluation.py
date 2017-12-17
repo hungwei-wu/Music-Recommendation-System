d41c1300441cd7856c48eb9f642904f97d5708ee
jasonwu82 <jasonwu082@gmail.com>
2017-12-16 22:19:52 -0500

    update evaluation

--- metrics/evaluation.py
+++ metrics/evaluation.py
@@ -1,19 +1,21 @@
-import numpy as np
+
 from collections import Counter
-import sys
-import csv
-#csv.field_size_limit(sys.maxint)
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def catlalog_coverage(total_music_size, recommend_music): # catalog coverage, defined as length of recommend music / total number of music
-    return len(recommend_music) / float(len(total_music_size))
+    recommend_music = sum(recommend_music,[])
+    count = Counter(recommend_music)
+    print("catalog coverage :{0}".format(len(count) / float(total_music_size)))
+    return len(count) / float(total_music_size)
 
 
 def RS_coverage_variation(recommend_music):  # To all user, repeat song times.  Defined as repeat times/total music length
+    recommend_music = sum(recommend_music,[])
     total_size = len(recommend_music)
     count = Counter(recommend_music)
-    print(len(count))
+    print ("RS_coverage :{0}".format(len(count)/float(total_size)))
     return len(count) / float(total_size)
 
 
@@ -30,7 +32,8 @@ def hit_count(recommend_music,num_in_user,user_list,user_index):
                     hit_count = hit_count + 1
                 else:
                     listen_count = listen_count + 1
-    return listen_count, (hit_count / (num_in_user)) , (len(user_list) - hit_count) / len(user_list)
+    return listen_count, (hit_count / (num_in_user)) , \
+           (len(user_list) / hit_count) / len(user_list) if hit_count != 0 else 1
 
 def hit(recommend_music,num_in_user,file,first_user,mode): # mode = 'uni' return unitary validation (use test set file), mode = 'nov' return novelty use tran + test set
     user_list = []
@@ -64,17 +67,35 @@ def hit(recommend_music,num_in_user,file,first_user,mode): # mode = 'uni' return
         hit_rate.append(hit)
         nov_rate.append(novelty)
 
-    #print(hit_rate)
-    print("avg of hit rate: {0}".format(sum(hit_rate)/len(hit_rate)))
-    #print(nov_rate)
-    print("avg of nov rate: {0}".format(sum(nov_rate) / len(nov_rate)))
     count = 0
     for i in listen_numbers:
-        #if(i >= 1):
-        #    print(count,i)
         count = count + 1
     hit_prob = sum(hit_rate) / len(hit_rate)
     if(mode == 'uni'):
+        print("avg of hit rate: {0}".format(sum(hit_rate) / len(hit_rate)))
         return hit_prob,listen_numbers
     elif(mode == 'nov'):
+        print("avg of nov rate: {0}".format(sum(nov_rate) / len(nov_rate)))
         return sum(nov_rate)/len(nov_rate),listen_numbers
+
+
+def diversity(recommend_music, vec_file):
+    df = pd.read_csv(vec_file, sep=',')
+    user_sim = []
+    for i in range(0,len(recommend_music)):
+        cos_sim = []
+        vec_list = read_vec(recommend_music[i],df)
+        for j in range(0,len(vec_list)):
+            for k in range(1,len(vec_list)):
+                if (k>j):
+                    cos_sim.append(cosine_similarity([vec_list[j],vec_list[k]])[0][1])
+        user_sim.append((sum(cos_sim)/len(cos_sim)))
+    print("avg of diversity :{0}".format(sum(user_sim) / len(user_sim)))
+    return (sum(user_sim) / len(user_sim))
+
+def read_vec(song_request,df):
+    res = []
+    for song in song_request:
+        song_w2v = df.loc[df['Unnamed: 0'] == str(song.encode('utf8'))].values.tolist()[0]
+        res.append(song_w2v[1:-1])
+    return res
